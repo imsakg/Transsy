a894db2de9d62ae8fc386be034d1d492078c362d
msa <msas@tuta.io>
2022-04-18 00:18:49 +0300

    added: usefull types header.
    Added types.h for useful some of data types.

---
+++ include/types.h
@@ -0,0 +1,22 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+enum COLOR
+{
+    red,
+    green,
+    blue,
+    cyan,
+    magenta,
+    yellow,
+    white
+};
+
+enum ANIMATIONS
+{
+    rainbow,
+    fade,
+    blink,
+};
+
+#endif // TYPES_H
