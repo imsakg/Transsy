a0cef57b65ac9f36bf545724e460979e14ecc2c0
msa <msas@tuta.io>
2022-04-18 00:15:34 +0300

    added: header file for pins
    Added pins.h which is contains wired pins of stick buttons, switches,
    potentiometer and RGB led.

---
+++ include/pins.h
@@ -0,0 +1,47 @@
+#define SW1_PIN_T1 22
+#define SW1_PIN_T2 32
+#define SW2_PIN_T1 25
+#define SW3_PIN_T1 27
+#define SW4_PIN_T1 31
+#define SW5_PIN_T1 29
+#define SW6_PIN_T1 33
+#define SW7_PIN_T1 35
+#define SW7_PIN_T2 37
+#define SW8_PIN_T1 39
+#define SW8_PIN_T2 41
+#define SW9_PIN_T1 43
+#define SW9_PIN_T2 45
+#define SW10_PIN_T1 5
+#define SW10_PIN_T2 6
+#define SW11_PIN_T1 44
+#define SW11_PIN_T2 A8
+
+#define RGB_B_PIN 49
+#define RGB_G_PIN 47
+#define RGB_R_PIN 48
+
+#define STICK1_PITCH_PIN A0
+#define STICK1_PITCH_ROLL A1
+#define STICK2_THROTTLE_PIN A2
+#define STICK2_YAW_PIN A3
+
+/*
+ sw1 = 22 / 32
+ sw2 = 25
+ sw3 = 27
+ sw4 = 31
+ sw5 = 29
+ sw6 = 33
+ sw7 = 35 / 37
+ sw8 = 39 / 41
+ sw9 = 43 / 45
+ sw10 = 5 / 6
+ sw11 = 44 / A8 # Change It later
+
+ btn1 = 13
+ btn2 = 17
+ btn3 = 15
+ btn4 = 19
+
+ # TODO : Wire Potentiometers pins
+ */
